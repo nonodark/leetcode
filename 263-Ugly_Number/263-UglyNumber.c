f6442b60c227a64852e957d226644267f01ab972
west <besterinulove@gmail.com>
2020-10-10 00:37:46 +0800

    [263] Ugly number. Ugly number

---
+++ 263-Ugly_Number/263-UglyNumber.c
@@ -0,0 +1,39 @@
+bool isUgly(int num){
+
+    if(num <= 0)
+    {
+        return false;
+    }
+        
+    
+    while(1)
+    {
+        if(num % 2 == 0)
+        {
+            num = num / 2;
+            continue;
+        }
+        
+        if(num % 3 == 0)
+        {
+            num = num / 3;
+            continue;
+        }
+        
+        if(num % 5 == 0)
+        {
+            num = num / 5;
+            continue;
+        } 
+        
+        if(num == 1)
+        {
+            return true;
+        }
+        else
+        {
+            
+            return false;
+        }
+    }        
+}
